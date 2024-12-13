deb797e820e96dcb38bb7108553fdd11bfa4a34f
zero.ryushin <zero.ryushin@bankle.co.kr>
2024-12-13 17:53:17 +0900

    ✨ 모바일 웹 환경에서 디바이스의 카메라 제어 기능

--- src/pages/Feed/FeedList/FeedList.jsx
+++ src/pages/Feed/FeedList/FeedList.jsx
@@ -44,7 +44,8 @@ const FeedList = ({ userInfo, defaultProfileImage }) => {
 
   /**
    * 반응형 모바일 웹에서 디바이스 네이티브 기능 접근해 카메라 촬영 후 미리보기 기능 및 갤러리 미저장 기능 구현
-   * @imgSrc: 갤러리에 저장되지 않고 찍힌 상태로 확대가 가능한 이미지 경로
+   * imgSrc: 갤러리에 저장되지 않고 찍힌 상태로 확대가 가능한 이미지 경로 > base64 파일 타입을 업로드 필요
+   * 추가 기능: pc 해상도에서는 파일 업로드 기능만 노출 > 두 기능 구현 후 해상도마다 조건부 노출 필요
    */
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
@@ -57,6 +58,8 @@ const FeedList = ({ userInfo, defaultProfileImage }) => {
     // facingMode: user(전면 카메라 제어) | environment(후면 카메라 제어)
     facingMode: "environment"
   }
+
+  console.log(imgSrc)
 
   return (
     <>
